2fe26812f3b517404ac36919eaf269a1ea5a3390
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-24 15:57:17 +0100

    feat(MENU): Añadir funcion aparte para el titulo.

--- quien_es_quien/paginas/menu_inicio.py
+++ quien_es_quien/paginas/menu_inicio.py
@@ -1,8 +1,11 @@
 import reflex as rx
 
+def titulo():
+    return rx.heading("¿Quién eis quién?",size="9")
+
 def jugar():
     return rx.button("JUGAR")
 
 def index() -> rx.Component:
     # Welcome Page (Index)
-    return rx.center(rx.heading(" "), rx.heading("¿Quién eis quién?",size="9"), jugar(), direction="column", align="center", spacing="9")
+    return rx.center(rx.heading(" "), titulo(), jugar(), direction="column", align="center", spacing="9")
