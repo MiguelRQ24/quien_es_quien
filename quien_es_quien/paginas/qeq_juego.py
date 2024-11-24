c6c31a3b4e127d71bc8a15bb322332ad5a2152c9
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-24 16:42:58 +0100

    feat(JUEGO): Añadir input a la pagina juego.

--- quien_es_quien/paginas/qeq_juego.py
+++ quien_es_quien/paginas/qeq_juego.py
@@ -1,4 +1,6 @@
 import reflex as rx
 
+def pregunta():
+    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica"), direction="row",)
 def juego():
-    return rx.text("pagina del juego")
+    return rx.center(pregunta(), )
