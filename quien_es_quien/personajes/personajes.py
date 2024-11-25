36655116b4015a6307fb41b76ee4aed60b3cfcee
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-25 17:41:50 +0100

    fix(PERSONAJES): Corregir nombre de constasnte.

--- quien_es_quien/personajes/personajes.py
+++ quien_es_quien/personajes/personajes.py
@@ -5,5 +5,5 @@ class robert(rx.State):
     # Crear la constante atributos del personaje
     ATRIBUTOS: tuple
     # Adjudicar atributos personaje
-    ATIBUTOS = ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
+    ATRIBUTOS = ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
     
