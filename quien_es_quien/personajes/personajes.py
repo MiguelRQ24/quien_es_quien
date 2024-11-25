ea93b42068e95fd689066d397eb278fba569ddab
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-25 17:53:32 +0100

    feat(PERSONAJES): Meter a los personajes en un diccionario.

--- quien_es_quien/personajes/personajes.py
+++ quien_es_quien/personajes/personajes.py
@@ -1,9 +1,5 @@
 import reflex as rx
 
-class robert(rx.State):
-
-    # Crear la constante atributos del personaje
-    ATRIBUTOS: tuple
-    # Adjudicar atributos personaje
-    ATRIBUTOS = ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
-    
+PERSONAJES = {
+    "Robert" : ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
+}
