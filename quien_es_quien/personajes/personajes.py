7dd94efb94bb370a5db0d5396ad20b34cf82af29
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-25 17:26:14 +0100

    feat(PERSONAJES): Crear el archivo personajes.

---
+++ quien_es_quien/personajes/personajes.py
@@ -0,0 +1,9 @@
+import reflex as rx
+
+class robert(rx.State):
+
+    # Crear la constante atributos del personaje
+    ATRIBUTOS: tuple
+    # Adjudicar atributos personaje
+    ATIBUTOS = ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
+    
