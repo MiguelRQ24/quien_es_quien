4b418e261b99310caf80f1a2a1823b2f1cd6faef
Alberto <karcernucle4rthrone@gmail.com>
2024-11-26 10:48:32 +0100

    feat(PERSONAJES): Añadir mas casos de personaje

--- quien_es_quien/personajes/personajes.py
+++ quien_es_quien/personajes/personajes.py
@@ -1,5 +1,31 @@
 import reflex as rx
 
-PERSONAJES = {
-    "Robert" : ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
-}
+class robert(rx.State):
+
+    # Crear la constante atributos del personaje
+    ATRIBUTOS: tuple
+    # Adjudicar atributos personaje
+    ATRIBUTOS = ("pelo marrón", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste")
+
+class susan(rx.State):
+
+    # Crear la constante atributos del personaje
+    ATRIBUTOS: tuple
+    # Adjudicar atributos personaje
+    ATRIBUTOS = ("pelo blanco", "nariz pequeña", "boca grande", "ojos castaños claros", "mofletes colorados", "aspecto risueño")
+
+
+class claire(rx.State):
+
+    # Crear la constante atributos del personaje
+    ATRIBUTOS: tuple
+    # Adjudicar atributos personaje
+    ATRIBUTOS = ("pelo naranja", "nariz pequeña", "boca pequeña", "ojos castaños", "sombrero de flores", "pendientes")
+
+class david(rx.State):
+
+    # Crear la constante atributos del personaje
+    ATRIBUTOS: tuple
+    # Adjudicar atributos personaje
+    ATRIBUTOS = ("pelo rubio", "nariz pequeña", "boca grande", "ojos castaño claro", "mofletes estirados", "aspecto serio")
+    
