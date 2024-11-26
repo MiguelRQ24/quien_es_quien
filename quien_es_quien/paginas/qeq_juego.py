c8caf2b80501b7ecea5ecc52ae04b188ce46c67d
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-26 10:59:17 +0100

    feat(JUEGO): Añadir boton dynoselect.

--- quien_es_quien/paginas/qeq_juego.py
+++ quien_es_quien/paginas/qeq_juego.py
@@ -1,8 +1,12 @@
 import reflex as rx
-
+import quien_es_quien.estado as est
+from reflex_dynoselect import dynoselect
+caracteristicas = [
+    {"value": "pelo marron", "label": "pelo marron"},
+]
 
 def pregunta():
-    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica"),  direction="row",)
+    return rx.hstack(rx.text("Tu personaje tiene:"), dynoselect(caracteristicas, placeholder="Caracteristica", search_placeholder="Busca caracteristica"), direction="row",)
 
 def juego():
     return rx.center(pregunta(), )
