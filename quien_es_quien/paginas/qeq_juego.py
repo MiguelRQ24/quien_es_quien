579aa059a919282002d47cfaae64cca589863254
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-26 12:56:07 +0100

    feat(Enviar_Caracteristica): guardar caracteristica.

--- quien_es_quien/paginas/qeq_juego.py
+++ quien_es_quien/paginas/qeq_juego.py
@@ -2,7 +2,7 @@ import reflex as rx
 import quien_es_quien.estado as est
 
 def pregunta():
-    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica"), rx.text(est.caracteristicas.caracteristica),  direction="row",)
+    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica", on_change=est.caracteristicas.ste_caracteristica), rx.text(est.caracteristicas.caracteristica),  direction="row",)
 
 def juego():
     return rx.center(pregunta(), )
