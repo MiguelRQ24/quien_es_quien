e44428f769f91468f6f9a9e90470892b1bc69389
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-26 12:54:35 +0100

    feat(CHEK): Añadir checkeo de variable caracteristica.

--- quien_es_quien/paginas/qeq_juego.py
+++ quien_es_quien/paginas/qeq_juego.py
@@ -2,7 +2,7 @@ import reflex as rx
 import quien_es_quien.estado as est
 
 def pregunta():
-    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica"),  direction="row",)
+    return rx.hstack(rx.text("Tu personaje tiene:"), rx.input(placeholder="Caracteristica"), rx.text(est.caracteristicas.caracteristica),  direction="row",)
 
 def juego():
     return rx.center(pregunta(), )
