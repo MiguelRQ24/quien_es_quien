b2372cb9ea4f6e2e599e20835dd7b0466f6bb4e4
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-28 19:04:28 +0100

    feat(IMAGENES): Añadir imagen de Susan a todos para probar.

--- quien_es_quien/estilos.py
+++ quien_es_quien/estilos.py
@@ -1,2 +1,2 @@
 import reflex as rx
-eliminado_estilo = dict(color_scheme="red")
+eliminado_estilo = dict(color_scheme=rx.color("red"))
--- quien_es_quien/paginas/qeq_juego.py
+++ quien_es_quien/paginas/qeq_juego.py
@@ -1,40 +1,42 @@
 import reflex as rx
 import quien_es_quien.estado as est
+import quien_es_quien.estilos as estilo
 
 def vista_persnajes():
-    return rx.grid( rx.cond(est.escoger.girados["susan"], rx.text("girado") , rx.text("Susan")),
-                    rx.cond(est.escoger.girados["robert"], rx.text("girado") , rx.text("Robert")),
-                    rx.cond(est.escoger.girados["claire"], rx.text("girado") , rx.text("Claire")),
-                    rx.cond(est.escoger.girados["david"], rx.text("girado") , rx.text("David")),
-                    rx.cond(est.escoger.girados["anne"], rx.text("girado") , rx.text("Anne")),
-                    rx.cond(est.escoger.girados["george"], rx.text("girado") , rx.text("George")),
-                    rx.cond(est.escoger.girados["joe"], rx.text("girado") , rx.text("Joe")),
-                    rx.cond(est.escoger.girados["anita"], rx.text("girado") , rx.text("Anita")),
-                    rx.cond(est.escoger.girados["bill"], rx.text("girado") , rx.text("Bill")),
-                    rx.cond(est.escoger.girados["alfred"], rx.text("girado") , rx.text("Alfred")),
-                    rx.cond(est.escoger.girados["max"], rx.text("girado") , rx.text("Max")),
-                    rx.cond(est.escoger.girados["tom"], rx.text("girado") , rx.text("Tom")),
-                    rx.cond(est.escoger.girados["alex"], rx.text("girado") , rx.text("Alex")),
-                    rx.cond(est.escoger.girados["sam"], rx.text("girado") , rx.text("Sam")),
-                    rx.cond(est.escoger.girados["richard"], rx.text("girado") , rx.text("Richard")),
-                    rx.cond(est.escoger.girados["paul"], rx.text("girado") , rx.text("Paul")),
-                    rx.cond(est.escoger.girados["maria"], rx.text("girado") , rx.text("Maria")),
-                    rx.cond(est.escoger.girados["frans"], rx.text("girado") , rx.text("Frans")),
-                    rx.cond(est.escoger.girados["philip"], rx.text("girado") , rx.text("Philip")),
-                    rx.cond(est.escoger.girados["eric"], rx.text("girado") , rx.text("Eric")),
-                    rx.cond(est.escoger.girados["peter"], rx.text("girado") , rx.text("Peter")),
-                    rx.cond(est.escoger.girados["herman"], rx.text("girado") , rx.text("Herman")), 
-                    rx.cond(est.escoger.girados["bernard"], rx.text("girado") , rx.text("Bernard")),
-                    rx.cond(est.escoger.girados["charles"], rx.text("girado") , rx.text("Charles")),
-                    columns="6", rows="4", spacing="3")
+    return rx.grid( rx.cond(est.escoger.girados["susan"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"), rx.text("Susan"), direction="column")),
+                    rx.cond(est.escoger.girados["robert"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Robert"))),
+                    rx.cond(est.escoger.girados["claire"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Claire"))),
+                    rx.cond(est.escoger.girados["david"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("David"))),
+                    rx.cond(est.escoger.girados["anne"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Anne"))),
+                    rx.cond(est.escoger.girados["george"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("George"))),
+                    rx.cond(est.escoger.girados["joe"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Joe"))),
+                    rx.cond(est.escoger.girados["anita"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Anita"))),
+                    rx.cond(est.escoger.girados["bill"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Bill"))),
+                    rx.cond(est.escoger.girados["alfred"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Alfred"))),
+                    rx.cond(est.escoger.girados["max"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Max"))),
+                    rx.cond(est.escoger.girados["tom"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Tom"))),
+                    rx.cond(est.escoger.girados["alex"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Alex"))),
+                    rx.cond(est.escoger.girados["sam"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Sam"))),
+                    rx.cond(est.escoger.girados["richard"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Richard"))),
+                    rx.cond(est.escoger.girados["paul"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Paul"))),
+                    rx.cond(est.escoger.girados["maria"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Maria"))),
+                    rx.cond(est.escoger.girados["frans"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Frans"))),
+                    rx.cond(est.escoger.girados["philip"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Philip"))),
+                    rx.cond(est.escoger.girados["eric"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Eric"))),
+                    rx.cond(est.escoger.girados["peter"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Peter"))),
+                    rx.cond(est.escoger.girados["herman"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Herman"))), 
+                    rx.cond(est.escoger.girados["bernard"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Bernard"))),
+                    rx.cond(est.escoger.girados["charles"], rx.text("girado", color_scheme="red") , rx.vstack(rx.image(src="/pjSusan.jpg", width="100px", height="auto"),rx.text("Charles"))),
+                    columns="8", rows="3", spacing="3")
 def pregunta():
-    return rx.vstack(rx.hstack(rx.text("Tu personaje tiene:"),
-                               rx.input(placeholder="Caracteristica", on_change=est.escoger.set_caracteristica), 
-                               rx.button("Enviar", on_click=est.escoger.validar),
-                               direction="row",
-                              ),
+    return rx.hstack(rx.text("Tu personaje tiene:"),
+                     rx.input(placeholder="Caracteristica", on_change=est.escoger.set_caracteristica, size="1" ),
+                     rx.button("Enviar", on_click=est.escoger.validar), 
                      rx.text(rx.cond(est.escoger.validacion, "", "caracteristica incorrecta, prueba otra vez")),
-                     )
+                     direction="row",
+                    )
+            
+                    
 
 def juego():
     return rx.center(rx.vstack(vista_persnajes(), pregunta(), direction="column" ))
