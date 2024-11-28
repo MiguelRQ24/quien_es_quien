8faba02081cfacad0c1970d4ccde82ca71b708fd
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-28 20:08:17 +0100

    feat(PERSONAJES): Actualizar caracteristicas de los personajes.

--- quien_es_quien/personajes/personajes.py
+++ quien_es_quien/personajes/personajes.py
@@ -1,28 +1,28 @@
 import reflex as rx
-caracteristicas = ["pelo negro", "pelo escaso", "nariz mediana", "pelo rubio", "pelo naranja", "boca pequeña", "ojos marrones", "pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto risueño", "aspecto triste", "pelo blanco", "nariz pequeña",  "mofletes estirados", "aspecto serio"]
+caracteristicas = ["pelo cataño oscuro", "pelo escaso", "nariz mediana", "pelo rubio", "pelirojo", "boca pequeña", "ojos marrones", "pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto risueño", "aspecto triste", "pelo blanco", "nariz pequeña",  "mofletes estirados", "aspecto serio"]
 personajes = {
     "robert": ["pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto triste"],
     "susan": ["pelo blanco", "nariz pequeña", "boca grande", "ojos marrones ", "mofletes colorados", "aspecto risueño"],
-    "claire": ["pelo naranja", "nariz pequeña", "boca pequeña", "ojos marrones", "sombrero de flores", "pendientes"],
+    "claire": ["pelirojo", "nariz pequeña", "boca pequeña", "ojos marrones", "sombrero de flores", "pendientes"],
     "david": ["pelo rubio", "nariz pequeña", "boca grande", "ojos marrones", "mofletes estirados", "aspecto serio"],
     "anne": ["pelo castaño", "nariz grande", "boca pequeña", "ojos marrones", "pendientes", "papada"],
     "george": ["pelo blanco", "nariz mediana", "boca grande", "ojos marrones", "mandibula prominente", "aspecto triste"],
     "joe": ["pelo rubio", "nariz mediana", "boca pequeña", "ojos marrones", "gafas", "aspecto risueño"],
     "anita": ["pelo rubio", "nariz pequeña", "boca pequeña", "ojos grises", "mofletes rojizos", "coletas"],
-    "bill": ["pelo escaso", "pelo naranja", "nariz pequeña", "boca pequeña", "ojos marrones", "mofletes abundantes", "aspecto risueño"],
-    "alfred":["pelo naranja", "nariz mediana", "boca pequeña", "ojos azules", "bigote", "aspecto serio"],
-    "max": ["pelo negro", "nariz grande", "boca grande", "ojos marrones", "bigote", "pelo rizado"],
-    "tom": ["pelo negro", "nariz pequeña", "boca pequeña", "ojos azules", "mofletes estirados", "pelo escaso"],
-    "alex": ["pelo negro", "nariz pequeña", "boca grande", "ojos marrones", "mandibula prominente", "bigote"],
+    "bill": ["pelo escaso", "pelirojo", "nariz pequeña", "boca pequeña", "ojos marrones", "mofletes abundantes", "aspecto risueño"],
+    "alfred":["pelirojo", "nariz mediana", "boca pequeña", "ojos azules", "bigote", "aspecto serio"],
+    "max": ["pelo cataño oscuro", "nariz grande", "boca grande", "ojos marrones", "bigote", "pelo rizado"],
+    "tom": ["pelo cataño oscuro", "nariz pequeña", "boca pequeña", "ojos azules", "mofletes estirados", "pelo escaso"],
+    "alex": ["pelo castaño oscuro", "nariz pequeña", "boca grande", "ojos marrones", "mandibula prominente", "bigote"],
     "sam": ["pelo blanco", "nariz mediana", "boca pequeña", "ojos marrones oscuro", "mofletes rellenos", "pelo escaso"],
     "richard": ["pelo castaño", "nariz mediana", "boca pequeña", "ojos marrones", "barba abundante", "pelo escaso"],
     "paul":["pelo blanco", "nariz pequeña", "boca pequeña", "ojos marrones", "mofletes estirados", "aspecto sereno"],
     "maria":["pelo castaño", "nariz pequeña", "boca mediana", "ojos marrones", "boina", "pendientes"],
-    "frans":["pelo naranja", "nariz pequeña", "boca pequeñas", "ojos marrones", "mofletes estirados", "cara cuadrada"],
+    "frans":["pelirojo", "nariz pequeña", "boca pequeñas", "ojos marrones", "mofletes estirados", "cara cuadrada"],
     "philip": ["pelo castaño oscuro", "nariz pequeña", "boca grande", "ojos marrones", "pelo ruloso", "mofletes colorados"],
     "eric":["pelo rubio", "nariz pequeña", "boca grande", "ojos marrones", "gorra", "cara cuadrada"],
     "peter": ["pelo blanco", "nariz grande", "boca grande", "ojos azules", "mofletes prominentes"],
-    "herman":["pelo naranja", "nariz grande", "boca pequeña", "ojos marrones", "mandibula pronunciada", "aspecto serio", "pelo escaso"],
+    "herman":["pelirojo", "nariz grande", "boca pequeña", "ojos marrones", "mandibula pronunciada", "aspecto serio", "pelo escaso"],
     "bernard":["pelo castaño", "nariz grande", "boca pequeña", "ojos marrones", "gorra", "aspecto serio"],
     "charles": ["pelo rubio", "nariz pequeña", "boca grande", "ojos marrones","aspecto feliz"]
 
