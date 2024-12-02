03c0dbae746fb6438912bf3ae15bfbc8fa8dc3d0
Alberto <karcernucle4rthrone@gmail.com>
2024-12-02 14:54:10 +0100

    refactor(comprobador_caracteristica): refactorizar el comprobador de caracteristicas

--- src/comprobar_caracteristica.py
+++ src/comprobar_caracteristica.py
@@ -1,9 +1,3 @@
 from quien_es_quien.personajes.personajes import caracteristicas 
 def caracteristica_valida(atributo):
-    if atributo in caracteristicas:
-        return True
-    else:
-        return False
-
-    # return "correcto" if rx.cond(personajes['anita'].__contains__(atributo)) else "caracteristica incorrecta, introduzca otra"
-
+    return atributo in caracteristicas
