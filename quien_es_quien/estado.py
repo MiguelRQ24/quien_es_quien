3872aa493a272a4efdfbf4a915d4b6722f581ff4
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-12-02 20:50:01 +0100

    refactor: Actualizar el estado para que las fotos se giren.

--- quien_es_quien/estado.py
+++ quien_es_quien/estado.py
@@ -5,10 +5,11 @@ from src.comprobar_caracteristica import caracteristica_valida
 from src.girar_personajes import girar_pjs
 from src.comprobador_personaje import comprobador_pers
 
-personajes = ('robert', 'susan', 'claire', 'david', 'anne', 'george', 'joe', 'anita', 'bill', 'alfred', 'max', 'tom', 'alex', 'sam', 'richard', 'paul', 'maria', 'frans', 'philip', 'eric', 'peter', 'herman', 'bernard', "charles")
+personajes = ['robert', 'susan', 'claire', 'david', 'anne', 'george', 'joe', 'anita', 'bill', 'alfred', 'max', 'tom', 'alex', 'sam', 'richard', 'paul', 'maria', 'frans', 'philip', 'eric', 'peter', 'herman', 'bernard', "charles"]
 
 class escoger(rx.State):
     personaje_escogido: str
+    girados: dict
     def jugar(self):
         return rx.redirect("/previa")
     
@@ -38,20 +39,16 @@ class escoger(rx.State):
         if self.validacion:
             self.personajes_girar += girar_pjs(self.caracteristica, self.personajes_girar, self.personaje_escogido)
         self.girar()
-
+        
     personajes_rectos: list
-    girados: dict = {
-                    "susan": False, 'robert': False, 'claire': False, 'david': False, 'anne': False, 'george': False, 
-                    'joe': False, 'anita': False, 'bill': False, 'alfred': False, 'max': False, 'tom': False, 'alex': False, 
-                    'sam': False, 'richard': False, 'paul': False, 'maria': False, 'frans': False, 'philip': False, 
-                    'eric': False, 'peter': False, 'herman': False, 'bernard': False
-                    }
+
+
     def girar(self):
         for personaje in self.personajes_girar:
             if personaje in self.personajes_rectos:
                 self.girados[personaje] = True
                 self.personajes_rectos.remove(personaje)
-    
+ 
     intento_acierto: str
     gano: bool 
     def comprobar_pj(self):
