d918263dad0bde48db69bca21cb9360902285a91
Alberto <karcernucle4rthrone@gmail.com>
2024-12-05 11:26:49 +0100

    test(comprobador_caracteristica): Añadir test letras mayusculas y minusculas

--- test/test_comprobar_caracteristicas.py
+++ test/test_comprobar_caracteristicas.py
@@ -1,8 +1,15 @@
 import pytest
 from src.comprobar_caracteristica import caracteristica_valida
 
+caracteristicas = ["gafas", "pelo castaño oscuro", "pelo escaso", "nariz mediana", "pelo rubio", "pelirojo", "boca pequeña", "ojos marrones", "pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto risueño", "aspecto triste", "pelo blanco", "nariz pequeña",  "mofletes estirados", "aspecto serio"]
+
 def test_caracteristica_erronea():
     assert not caracteristica_valida("sdaf")
 
+def test_caracteristica_vacia():
+    assert not caracteristica_valida("")
+
 def test_caracteristica():
-    assert caracteristica_valida("pelo blanco")
+    assert caracteristica_valida("Pelo blanco")
+    for caracteristica in caracteristicas:
+        assert caracteristica_valida(caracteristica)
