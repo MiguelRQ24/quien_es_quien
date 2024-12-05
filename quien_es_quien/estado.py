859bf3511bf7fa07f03da4ee5ee95d044faf8e21
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-12-05 10:20:58 +0100

    refactor(estado.py): Mover todas las variables del estado al comienzo de este.

--- quien_es_quien/estado.py
+++ quien_es_quien/estado.py
@@ -7,9 +7,16 @@ from src.comprobador_personaje import comprobador_pers
 
 personajes = ['robert', 'susan', 'claire', 'david', 'anne', 'george', 'joe', 'anita', 'bill', 'alfred', 'max', 'tom', 'alex', 'sam', 'richard', 'paul', 'maria', 'frans', 'philip', 'eric', 'peter', 'herman', 'bernard', "charles"]
 
-class escoger(rx.State):
+class estado(rx.State):
     personaje_escogido: str
     girados: dict
+    caracteristica: str
+    validacion: bool
+    personajes_rectos: list
+    personajes_girar: list
+    intento_acierto: str
+    gano: bool 
+
     def jugar(self):
         return rx.redirect("/previa")
     
@@ -31,29 +38,21 @@ class escoger(rx.State):
                     'eric': False, 'peter': False, 'herman': False, 'bernard': False
                         }
         
-    caracteristica: str
-    validacion: bool
     def validar(self): 
         self.validacion = caracteristica_valida(self.caracteristica)
         self.cual_girar()
 
-    personajes_girar: list
     def cual_girar(self):
         if self.validacion:
             self.personajes_girar += girar_pjs(self.caracteristica, self.personajes_girar, self.personaje_escogido)
             self.girar()
         
-    personajes_rectos: list
-
-
     def girar(self):
         for personaje in self.personajes_girar:
             if personaje in self.personajes_rectos:
                 self.girados[personaje] = True
                 self.personajes_rectos.remove(personaje)
  
-    intento_acierto: str
-    gano: bool 
     def comprobar_pj(self):
         self.gano = comprobador_pers(self.personaje_escogido, self.intento_acierto)
            
