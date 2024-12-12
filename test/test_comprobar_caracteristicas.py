01d02e7eb0c3ee1b98aeeb500890f1ed059eb04e
Alberto <karcernucle4rthrone@gmail.com>
2024-12-12 10:10:58 +0100

    fix(PERSONAJES): :fire: cambiar caracteristica de género

--- test/test_comprobar_caracteristicas.py
+++ test/test_comprobar_caracteristicas.py
@@ -1,10 +1,12 @@
 import pytest
 from src.comprobar_caracteristica import caracteristica_valida
 
-caracteristicas = ["mujer", "hombre", "sombrero", "perilla", "gafas", "pelo castaño oscuro", "pelo escaso", "nariz pequeña", "pelo rubio", "pelirojo", "boca pequeña", "ojos marrones", "pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto risueño", "aspecto triste", "pelo blanco",  "aspecto serio", "bigote"]
+caracteristicas = ["sexo femenino", "sexo masculino", "sombrero", "perilla", "gafas", "pelo castaño oscuro", "pelo escaso", "nariz pequeña", "pelo rubio", "pelirojo", "boca pequeña", "ojos marrones", "pelo castaño", "nariz grande", "boca grande", "ojos azules", "mofletes colorados", "aspecto risueño", "aspecto triste", "pelo blanco",  "aspecto serio", "bigote"]
 
 def test_caracteristica_erronea():
     assert not caracteristica_valida("sdaf")
+
+    assert not caracteristica_valida("42069")
 
 def test_caracteristica_vacia():
     assert not caracteristica_valida("")
